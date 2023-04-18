a0a7cddcac1d2388810dbd7422eac93fb5b6d86a
DESKTOP-A6FKTJ2\Ahmed <ginryoix@gmail.com>
2023-04-18 16:25:55 +0200

    basic done

--- src/Basic.js
+++ src/Basic.js
@@ -1,46 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-const url = 'https://serverless-functions-course.netlify.app/api/2-basic-api'
-const Basic = () => {
-  const [products, setProducts] = useState([])
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+const url = 'https://serverless-fncs-gryo.netlify.app/api/2-basic-api';
 
+const Basic = () => {
+  const [products, setProducts] = useState([]);
   const fetchData = async () => {
     try {
-      const { data } = await axios.get(url)
-      setProducts(data)
+      const { data } = await axios(url);
+      setProducts(data);
     } catch (error) {}
-  }
+  };
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
+
   return (
-    <section className='section section-center'>
-      <div className='title'>
-        <h2>basic setup</h2>
-        <div className='title-underline'></div>
+    <section className="section section-center">
+      <div className="title">
+        <h2>Basic</h2>
+        <div className="title-underline"></div>
       </div>
-      <div className='products'>
-        {products.map((product) => {
+      <div className="products">
+        {products.map((prod) => {
+          console.log(prod);
           const {
             id,
             image: { url },
             price,
             name,
-          } = product
+          } = prod;
           return (
-            <article className='product' key={id}>
+            <article className="product" key={id}>
               <img src={url} alt={name} />
-              <div className='info'>
+              <div className="info">
                 <h5>{name}</h5>
-                <h5 className='price'>${price}</h5>
+                <h5 className="price">${price}</h5>
               </div>
             </article>
-          )
+          );
         })}
       </div>
     </section>
-  )
-}
-
-export default Basic
+  );
+};
+export default Basic;
